0a28706864755ebaa9576872d73bb176a5ee12a4
Vidipt <vidipt.khetriwal2020@vitstudent.ac.in>
2023-04-11 22:31:27 +0530

    ✨tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <p className="">Welcome</p>
+    <div className="App w-screen h-screen bg-red-300">
+      <div className="text-3xl font-bold underline text-center">Welcome</div>
     </div>
   );
 }
