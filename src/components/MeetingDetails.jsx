8da59c25c417ad3dfa23b0306dd44b8327eda384
Vidipt <vidipt.khetriwal2020@vitstudent.ac.in>
2023-04-14 18:48:21 +0530

    🚧Added react-quilljs

--- src/components/MeetingDetails.jsx
+++ src/components/MeetingDetails.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { useQuill } from "react-quilljs";
+import "quill/dist/quill.snow.css";
 
 import fontSize from "../assets/icons/fontSize.png";
 import alignLeft from "../assets/icons/alignLeft.png";
@@ -13,12 +15,38 @@ import notes from "../assets/icons/notes.png";
 import danger from "../assets/icons/danger.png";
 
 export default function MeetingDetails() {
+  const modules = {
+    toolbar: [
+      [{ size: ["small", "medium", "large"] }],
+      [{ align: [] }, { list: "bullet" }, { color: [] }, "link"],
+      ["bold"],
+    ],
+  };
+
+  const { quill, quillRef } = useQuill({ modules });
+  const [value, setValue] = useState();
+
+  React.useEffect(() => {
+    if (quill) {
+      quill.clipboard.dangerouslyPasteHTML(
+        "<br/> <h2> 🎯 <strong> Goal: </strong> </h2> <br/> <h3> Accept Our new Logo Proposal</h3><br/> <h2> 📝 <strong> Notes: </strong> </h2> <br/> <h3> - the shape and font are already very good</h3> <h3> - the still need to work on the color palette</h3> <h3> - the back&white is very satisfying, but the color is not</h3><br/> <h2> ⏰ <strong> Deadline: </strong> </h2> <br/> <h3> July 21</h3>"
+      );
+      quill.on("text-change", () => {
+        console.log(quillRef.current.firstChild.innerHTML);
+        setValue(quillRef.current.firstChild.innerHTML);
+      });
+    }
+  }, [quill]);
+
   return (
     <div className="h-full border-l-grey border-l-2 border-l-solid p-2">
       <div className="w-full p-4 text-3xl text-gray-700 font-bold border-b-2 border-b-solid border-b-grey">
         Meeting Minutes
       </div>
-      <div className="w-full p-4 flex justify-around border-b-2 border-b-solid border-b-grey">
+      <div className="w-full h-7/10">
+        <div ref={quillRef} className="w-full h-full border-none" />
+      </div>
+      {/* <div className="w-full p-4 flex justify-around border-b-2 border-b-solid border-b-grey">
         <div className="w-1/10">
           <img src={fontSize} alt="Font Size" />
         </div>
@@ -78,7 +106,7 @@ export default function MeetingDetails() {
         </div>
         <div className="font-bold text-lg">Deadline:</div>
         <div className="text-md">&nbsp;July 21</div>
-      </div>
+      </div> */}
     </div>
   );
 }
