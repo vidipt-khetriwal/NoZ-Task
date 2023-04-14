c3e4dce1ffcb36b3530f64020bee746a202c99cb
Vidipt <vidipt.khetriwal2020@vitstudent.ac.in>
2023-04-14 19:19:23 +0530

    🐛Deployment Bugs

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,13 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     include: ["react-quill"],
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -5,5 +15,17 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     include: ["react-quill"],
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+    },
+    outDir: "build",
+    emptyOutDir: true,
+    webpack: {
+      config: require("./webpack.config.js"),
+    },
   },
 });
